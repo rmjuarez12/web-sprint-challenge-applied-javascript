3976e571241abb805ae3a3bb001490ee8ce8b3af
rmjuarez12 <rmjuarez12@gmail.com>
2020-09-11 21:50:57 -0400

    Finished with the tabs component

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,47 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+// Import Dependencies
+import axios from "axios";
+
+// Get the data from API Endpoint
+const APIUrl = "https://lambda-times-api.herokuapp.com/topics";
+const getTabs = axios.get(APIUrl);
+
+// Create a Promise
+getTabs
+  .then((res) => {
+    // Get the tabs from the object
+    const allTabs = res.data.topics;
+
+    // Get the DOM element that we will append the tabs to
+    const tabDomElement = document.querySelector("div.topics");
+
+    // Use the tabCreator component to append each tab
+    allTabs.forEach((tab) => {
+      const newTab = tabCreator(tab);
+
+      // Add each tab to the DOM
+      tabDomElement.appendChild(newTab);
+    });
+
+    // console.log to ensure we are getting data;
+    console.log(res);
+  })
+  .catch((error) => console.log(error));
+
+// Component for creating tab elements
+function tabCreator(tab) {
+  // Create the main tab container
+  const tabContainer = document.createElement("div");
+
+  // Add attributes to the main container
+  tabContainer.classList.add("tab");
+
+  // Add the content for the tab
+  tabContainer.textContent = tab;
+
+  // Return the tab element
+  return tabContainer;
+}
